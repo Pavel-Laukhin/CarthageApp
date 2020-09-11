6d746bd30c3441d3300ccbe0cdcb77ae2220a35a
Pavel Laukhin <6886834@gmail.com>
2020-09-11 15:58:41 +0300

    Button corners have been rounded and the title has been set up!

--- CarthageApp/ViewController.swift
+++ CarthageApp/ViewController.swift
@@ -10,18 +10,23 @@ import UIKit
 import Kingfisher
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var gitHubLogo: UIImageView!
+    @IBOutlet weak var signInButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Устанавливаю логотип ГитХаба по ссылке
         let url = URL(string: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png")
         gitHubLogo.kf.setImage(with: url)
         
+        // Закругляю края у кнопки:
+        signInButton.layer.cornerRadius = 5
         
+        // Устанавливаю титульное название:
+        title = "GitHub.com"
     }
-
 
 }
 
