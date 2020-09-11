ce2c760a58d23c7def9b19546941103a93dd6169
Pavel Laukhin <6886834@gmail.com>
2020-09-11 13:47:52 +0300

    Initial Commit

---
+++ CarthageApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarthageApp
+//
+//  Created by Павел on 11.09.2020.
+//  Copyright © 2020 Pavel Laukhin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
